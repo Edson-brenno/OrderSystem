9bb43f321e2bd57b3572f5fb000a4394d66ed6c7
brenno <brennorebeiro@gmail.com>
2024-07-17 21:42:10 -0300

    Add OrderItem service

---
+++ src/main/java/com/spring/Service/OrderItemService.java
@@ -0,0 +1,19 @@
+package com.spring.Service;
+
+import com.spring.Entities.OrderItem;
+import com.spring.Repository.OrderItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderItemService {
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    public List<OrderItem> findAll() {
+        return orderItemRepository.findAll();
+    }
+}
